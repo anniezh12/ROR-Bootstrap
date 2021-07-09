0b70e03d29a0e820dab8db15c918a962ae6f98e0
Aniqa Elahi <aniqaelahi@yahoo.com>
2021-07-09 14:34:54 -0700

    Adding font-awesome gem

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get 'welcome/aboutus'
   get 'welcome/contactus'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'welcome#index'
 end
