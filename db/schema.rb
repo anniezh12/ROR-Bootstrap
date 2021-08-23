bd763dd0ebd67d3e3ecd91813049442486cfdd0d
Aniqa Elahi <aniqaelahi@yahoo.com>
2021-08-22 21:51:59 -0700

    test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_10_052510) do
+ActiveRecord::Schema.define(version: 2021_07_16_184849) do
 
   create_table "foods", force: :cascade do |t|
     t.string "name"
@@ -34,6 +34,9 @@ ActiveRecord::Schema.define(version: 2021_07_10_052510) do
     t.string "unconfirmed_email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin"
+    t.string "firstname"
+    t.string "lastname"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
